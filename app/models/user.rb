991d79fa91c5fc58a6abb351680f51155753679f
shimaz <shimaz@shimaznoMacBook-Pro.local>
2026-02-12 00:15:57 +0900

    add: ユーザのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_one :sugar_glider, dependent: :destroy
+
+  validates :name, presence: true
 end
